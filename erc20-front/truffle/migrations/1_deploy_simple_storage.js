1561ec57af4f03d25a3a51037a244e242908323e
formegusto <formegusto@gmail.com>
2022-12-02 16:35:42 +0900

    crowd sale deployed

--- erc20-front/truffle/migrations/1_deploy_simple_storage.js
+++ erc20-front/truffle/migrations/1_deploy_simple_storage.js
@@ -1,10 +1,17 @@
 const Erc20Token = artifacts.require("Erc20Token");
 const Sale = artifacts.require("Sale");
 
-module.exports = function (deployer) {
+module.exports = async function (deployer, network, accounts) {
+  /*
+  network : 사용하고 있는 네트워크 정보
+  accounts : 네트워크의 계정 갯수
+  */
   const tokenName = "Done";
   const tokenSymbol = "DO";
-  const totalSupply = 10;
+  const totalSupply = web3.utils.toWei("100", "ether");
 
   deployer.deploy(Erc20Token, tokenName, tokenSymbol, totalSupply);
+
+  const DOToken = await Erc20Token.deployed();
+  deployer.deploy(Sale, 1, accounts[0], DOToken.address);
 };
